1f28c5fbb1eb2a4dafec7002c17944857487d193
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-04-21 16:44:26 +0300

    Add group folder Source and create two files "Syntax.h"  "Syntax.m»

---
+++ SyntaxObj-C/SyntaxObj-C/Source/Syntax.m
@@ -0,0 +1,13 @@
+//
+//  Syntax.m
+//  SyntaxObj-C
+//
+//  Created by Aleksandr Poliakov on 21.04.17.
+//  Copyright © 2017 Aleksandr Poliakov. All rights reserved.
+//
+
+#import "Syntax.h"
+
+@implementation Syntax
+
+@end
---
+++ SyntaxObj-C/SyntaxObj-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SyntaxObj-C
+//
+//  Created by Aleksandr Poliakov on 21.04.17.
+//  Copyright © 2017 Aleksandr Poliakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Syntax.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
